e7c3db051aac945e7e587b09257d7c7de2fa9081
allimacmillan <alli.macmillan@comcast.net>
2016-02-20 19:25:18 -0700

    Added function definitions

--- BackEnd.py
+++ BackEnd.py
@@ -9,4 +9,17 @@ adam = User('Adam', 'adam@colorado.edu', 'CSCI')
 print (adam.name)
 print (adam.email)
 print (adam.studying)
+
 '''
+def  addNewUser(_name, _email, _topic):
+  # make instance of class using parameters
+  
+def checkTopicArray(_topic)
+  #return address if in array
+  #return address of next open space if not in array and add topic to that address
+  #array doubling if needed
+
+def findTail(_head)
+
+def addTopicArray(_address)
+  #add user to linked-list associated with return address from checkTopicArray fcn
