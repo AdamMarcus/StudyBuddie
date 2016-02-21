07cd656b600fd5173c8c8dfdb062b9f81d176647
Marcus Van Clief <cptnjklop@gmail.com>
2016-02-20 18:33:28 -0700

    Added the User Class

--- BackEnd.py
+++ BackEnd.py
@@ -1,3 +1,12 @@
 # This is the back end file
-# class User:
-  
+class User:
+	def __init__(self, name, email, studying):
+		self.name = name
+		self.email = email
+		self.studying = studying
+'''
+adam = User('Adam', 'adam@colorado.edu', 'CSCI')
+print (adam.name)
+print (adam.email)
+print (adam.studying)
+'''
