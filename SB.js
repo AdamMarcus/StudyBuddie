7eb9a666cf96fb4a7e827efc77b44c66932e760e
ByronBecker <byron.becker@colorado.edu>
2016-02-21 15:49:26 -0700

    All Files

---
+++ SB.js
@@ -0,0 +1,15 @@
+console.log("hello")
+
+
+$("#droplist").click(function(timing){
+    
+//$ calls id
+//    .hover gives the hover method
+// function of how we are hovering
+    
+$(".dropdownclick").toggleClass("is-visible");
+    
+});
+
+
+
